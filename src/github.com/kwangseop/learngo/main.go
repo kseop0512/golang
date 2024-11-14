02dad39d33ec630c58f5429d53718b3822697356
김광섭 <kseop@Seop-MacBookPro.local>
2024-11-14 13:00:09 +0900

    golang Function part Two

--- src/github.com/kwangseop/learngo/main.go
+++ src/github.com/kwangseop/learngo/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+// func repeatMe(words ...string) {
+// 	fmt.Println(words)
+// }
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
 func main() {
+	// repeatMe("nico", "lynn", "dal", "marl", "flynn")
+	totalLength, up := lenAndUpper("kseop")
+	fmt.Println(totalLength, up)
 
 }
