2a2a770abf29c538949c45a7991d02edff237dfd
kseop <kseop0512@gmail.com>
2024-12-02 19:36:22 +0900

    golang Goroutines

--- src/github.com/kwangseop/learngo/main.go
+++ src/github.com/kwangseop/learngo/main.go
@@ -206,3 +206,24 @@ func learngoMain() {
 		fmt.Println(word)
 	}
 }
+
+/*
+// Goroutines
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
+}
+
+
+func main() {
+	go sexyCount("seop")
+	go sexyCount("kwangseop")
+	time.Sleep(time.Second * 5)
+}
+*/
+
+func main() {
+
+}
