7806b061127a53b18345cfc06740129fa783c55f
kseop <kseop0512@gmail.com>
2024-12-05 12:41:14 +0900

    golang getPages

--- src/github.com/kwangseop/learngo/getPages/main.go
+++ src/github.com/kwangseop/learngo/getPages/main.go
@@ -4,18 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+// var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
+// https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=python&recruitPage=1&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n
+// https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=python&recruitPage=2&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n
 func main() {
-	getPages()
+
+	totalPages := getPages()
+
+	for i := 1; i < totalPages; i++ {
+		getPage(i)
+	}
+	fmt.Println(totalPages)
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
+	/*
+		req, rErr := http.NewRequest("GET", baseURL, nil)
+
+		checkErr(rErr)
+
+		// 프록시로 호출
+		purl, err := url.Parse(baseURL)
+		client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(purl)}}
+		res, err := client.Do(req)
+	*/
 	checkErr(err)
 	checkCode(res)
 
@@ -23,8 +48,13 @@ func getPages() int {
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
-	fmt.Println(doc)
-	return 0
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+
+	// fmt.Println(doc)
+	return pages
 }
 
 func checkErr(err error) {
