e76a2d51d82ec5ce6bddc272352f60ff6ddb1f5d
kseop <kseop0512@gmail.com>
2024-12-11 20:41:51 +0900

    golang getPage struct

--- src/github.com/kwangseop/learngo/getPages/main.go
+++ src/github.com/kwangseop/learngo/getPages/main.go
@@ -5,9 +5,16 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+}
 
 // var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
@@ -15,17 +22,52 @@ var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchwo
 // https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=python&recruitPage=1&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n
 // https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=python&recruitPage=2&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n
 func main() {
-
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 1; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
-	fmt.Println(totalPages)
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+	title := cleanString(card.Find(".area_job>h2.job_tit>a>span").Text())
+	location := cleanString(card.Find(".job_condition>span:first-child").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+	}
+
+}
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
